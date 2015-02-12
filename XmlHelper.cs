9451d208cb781257e956e50ee7760523a8426e8e
xuhang <xuhang1108@126.com>
2015-02-12 10:33:02 +0800

    add xml\xslt\webrequest helper class

---
+++ XmlHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Reflection;
+
+namespace Util
+{
+    public class XmlHelper
+    {
+        /// <summary>
+        /// XML String 序列化对象成字符串
+        /// </summary>
+        public static string XmlSerialize<T>(T obj)
+        {
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+            string xmlString = string.Empty;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //XmlWriterSettings settings = new XmlWriterSettings();
+                //settings.OmitXmlDeclaration = true;
+                //XmlWriter xmlWriter = XmlWriter.Create(ms, settings);
+
+                xmlSerializer.Serialize(ms, obj, ns);
+                xmlString = Encoding.UTF8.GetString(ms.ToArray());
+            }
+            return xmlString;
+        }
+
+
+      
+        /// <summary>
+        /// XML String 反序列化成对象
+        /// </summary>
+        public static T XmlDeserialize<T>(string xmlString)
+        {
+            T t = default(T);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (Stream xmlStream = new MemoryStream(Encoding.UTF8.GetBytes(xmlString)))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(xmlStream))
+                {
+                    Object obj = xmlSerializer.Deserialize(xmlReader);
+                    t = (T)obj;
+                }
+            }
+            return t;
+        }
+
+        
+       
+    }
+}
---
+++ XsltHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Xsl;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Util
+{
+    /// <summary>
+    /// xslt文件模板转html heler class
+    /// </summary>
+    public class XsltHelper
+    {
+
+        public static string Transform(string xsltPath, Dictionary<string, string> argumentDic)
+        {
+            XslCompiledTransform transform = new XslCompiledTransform();
+            transform.Load(xsltPath);
+            XsltArgumentList arglist = new XsltArgumentList();
+            foreach (var d in argumentDic)
+            {
+                arglist.AddParam(d.Key, string.Empty, d.Value);
+            }
+
+            using (XmlReader xml = XmlReader.Create(new StringReader("<?xml version=\"1.0\" encoding=\"utf-8\" ?><root></root>")))
+            using (MemoryStream reader = new MemoryStream())
+            {
+                transform.Transform(xml, arglist, reader);
+                return Encoding.UTF8.GetString(reader.ToArray());
+
+            }
+
+        }
+
+        /// <summary>
+        /// xslt文件转html,支持html table
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xsltPath"></param>
+        /// <param name="argumentDic"></param>
+        /// <param name="tableArgs">html table数据</param>
+        /// <returns></returns>
+        public static string Transform<T>(string xsltPath, Dictionary<string, string> argumentDic, List<T> tableArgs)
+        {
+            XslCompiledTransform transform = new XslCompiledTransform();
+            transform.Load(xsltPath);
+            XsltArgumentList arglist = new XsltArgumentList();
+            foreach (var d in argumentDic)
+            {
+                arglist.AddParam(d.Key, string.Empty, d.Value);
+            }
+
+            MemoryStream sm = new MemoryStream(XmlSerialize(tableArgs));
+
+            using (XmlReader xml = XmlReader.Create(sm))
+            using (MemoryStream reader = new MemoryStream())
+            {              
+                transform.Transform(xml, arglist, reader);
+                sm.Close();
+                return Encoding.UTF8.GetString(reader.ToArray());
+            }
+
+        }
+
+
+        public static byte[] XmlSerialize<T>(T obj)
+        {
+
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+            string xmlString = string.Empty;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute("root"));
+            using (MemoryStream ms = new MemoryStream())
+            {
+
+                xmlSerializer.Serialize(ms, obj, ns);
+                return ms.ToArray();
+            }
+
+        }
+    }
+}
